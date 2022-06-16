085a2c499feaa81d9895938d8a7e108349c33257
JosebiasNeto <josebiasbsneto@gmail.com>
2022-06-16 18:04:21 -0300

    build profile screen

--- lib/pages/AccountPage/profile.dart
+++ lib/pages/AccountPage/profile.dart
@@ -3,7 +3,7 @@ import 'package:mps_driver_app/theme/CustomIcon.dart';
 import '../../models/Driver.dart';
 import '../../theme/app_colors.dart';
 
-class ProfilePage extends StatelessWidget{
+class ProfilePage extends StatelessWidget {
   const ProfilePage();
 
   @override
@@ -12,14 +12,12 @@ class ProfilePage extends StatelessWidget{
   }
 }
 
-class ProfilePageState extends StatefulWidget{
-
+class ProfilePageState extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePageState>{
-
+class _ProfilePageState extends State<ProfilePageState> {
   Driver mark = Driver(
     "marklarson",
     "Mark",
@@ -34,108 +32,130 @@ class _ProfilePageState extends State<ProfilePageState>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: 250,
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                  image: DecorationImage(image:
-                  AssetImage('assets/images/background_profile_img.png'),
-                      fit: BoxFit.cover)
-              ),
-              child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Column(children: [
-                      SizedBox(height: 85),
-                      Container(
-                        padding: EdgeInsets.all(2),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(80),
-                            border: Border.all(width: 1, color: App_Colors.grey_light.value)
-                        ),
-                        child: Icon(Icons.person, size: 90, color: App_Colors.grey_light.value),
-                      ),
-                      SizedBox(height: 15),
-                      Text(mark.firstName + " " + mark.lastName,
-                        style: TextStyle(fontSize: 18, color: Colors.white),),
-                      Text("Driver", style: TextStyle(fontSize: 12,
-                          color: App_Colors.grey_light.value))
-                      ],
-                    ),
-                  ])
-            ),
-            SizedBox(height: 18),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+        body: Column(children: [
+      Container(
+          height: 250,
+          width: double.infinity,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/background_profile_img.png'),
+                  fit: BoxFit.cover)),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Column(
               children: [
-              getInfoColumn(mark.trips.toString(), "Trips"),
-              getInfoColumn(mark.month.toString(), "Month"),
-              getInfoColumn(mark.deliveries.toString(), "Deliveries"),
-              getInfoColumn(mark.rating.toString(), "Rating"),
-            ]),
-            SizedBox(height: 22),
-            Container(color: App_Colors.grey_background.value,
-              padding: EdgeInsets.only(left: 25, top: 5, right: 25, bottom: 5),
-              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("Profile", style: TextStyle(fontSize: 16,
-                fontWeight: FontWeight.w500),),
-                Text("Edit", style: TextStyle(fontSize: 12),)
-              ])),
-            SizedBox(height: 15),
-            getInfoRow("Username", mark.userName),
-            Divider(thickness: 1, color: App_Colors.grey_light.value),
-            getInfoRow("First name", mark.firstName),
-            Divider(thickness: 1, color: App_Colors.grey_light.value),
-            getInfoRow("Last name", mark.lastName),
-            Divider(thickness: 1, color: App_Colors.grey_light.value),
-            getInfoRow("Email", mark.email),
-            SizedBox(height: 20),
-            Container(color: App_Colors.grey_background.value,
-                padding: EdgeInsets.only(left: 25, top: 5, bottom: 5),
-                child: Row(children: [
-                      Text("Account", style: TextStyle(fontSize: 16,
-                          fontWeight: FontWeight.w500),),
-                    ])),
-            SizedBox(height: 25),
-            GestureDetector(child: Container(padding: EdgeInsets.only(left: 25),
-                child: Row(children: [
-                  Text("Change password", style:
-                  TextStyle(fontSize: 14)),
-                ])),
-              onTap: (){},
+                SizedBox(height: 85),
+                Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(80),
+                      border: Border.all(
+                          width: 1, color: App_Colors.grey_light.value)),
+                  child: Icon(Icons.person,
+                      size: 90, color: App_Colors.grey_light.value),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  mark.firstName + " " + mark.lastName,
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+                Text("Driver",
+                    style: TextStyle(
+                        fontSize: 12, color: App_Colors.grey_light.value))
+              ],
             ),
-            SizedBox(height: 25),
-            GestureDetector(child: Container(padding: EdgeInsets.only(left: 25),
-                child: Row(children: [
-                    Text("Logout", style:
-                    TextStyle(color: App_Colors.alert_color.value, fontSize: 14)),
-                ])),
-              onTap: (){},
+          ])),
+      SizedBox(height: 18),
+      Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        getInfoColumn(mark.trips.toString(), "Trips"),
+        getInfoColumn(mark.month.toString(), "Month"),
+        getInfoColumn(mark.deliveries.toString(), "Deliveries"),
+        getInfoColumn(mark.rating.toString(), "Rating"),
+      ]),
+      SizedBox(height: 22),
+      Container(
+          color: App_Colors.grey_background.value,
+          padding: EdgeInsets.only(left: 25, top: 5, right: 25, bottom: 5),
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+            Text(
+              "Profile",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
             ),
-          ])
-    );
+            Text(
+              "Edit",
+              style: TextStyle(fontSize: 12),
+            )
+          ])),
+      SizedBox(height: 15),
+      getInfoRow("Username", mark.userName),
+      Divider(thickness: 1, color: App_Colors.grey_light.value),
+      getInfoRow("First name", mark.firstName),
+      Divider(thickness: 1, color: App_Colors.grey_light.value),
+      getInfoRow("Last name", mark.lastName),
+      Divider(thickness: 1, color: App_Colors.grey_light.value),
+      getInfoRow("Email", mark.email),
+      SizedBox(height: 20),
+      Container(
+          color: App_Colors.grey_background.value,
+          padding: EdgeInsets.only(left: 25, top: 5, bottom: 5),
+          child: Row(children: [
+            Text(
+              "Account",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+          ])),
+      SizedBox(height: 25),
+      GestureDetector(
+        child: Container(
+            padding: EdgeInsets.only(left: 25),
+            child: Row(children: [
+              Text("Change password", style: TextStyle(fontSize: 14)),
+            ])),
+        onTap: () {},
+      ),
+      SizedBox(height: 25),
+      GestureDetector(
+        child: Container(
+            padding: EdgeInsets.only(left: 25),
+            child: Row(children: [
+              Text("Logout",
+                  style: TextStyle(
+                      color: App_Colors.alert_color.value, fontSize: 14)),
+            ])),
+        onTap: () {},
+      ),
+    ]));
   }
-  getInfoColumn(String value, String label){
+
+  getInfoColumn(String value, String label) {
     return Column(children: [
-      Text(value, style: TextStyle(
-          color: App_Colors.black_text.value, fontFamily: 'Poppins',
-          fontWeight: FontWeight.w500, fontSize: 20
-      ),),
+      Text(
+        value,
+        style: TextStyle(
+            color: App_Colors.black_text.value,
+            fontFamily: 'Poppins',
+            fontWeight: FontWeight.w500,
+            fontSize: 20),
+      ),
       SizedBox(height: 10),
       Text(label, style: TextStyle(fontSize: 14))
     ]);
   }
-  getInfoRow(String label, String value){
+
+  getInfoRow(String label, String value) {
     return Container(
       padding: EdgeInsets.only(left: 25, right: 25, top: 10, bottom: 3),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        Text(label, style: TextStyle(fontSize: 14),),
-        Text(value, style: TextStyle(fontSize: 14,
-        fontWeight: FontWeight.w500))],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 14),
+          ),
+          Text(value,
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500))
+        ],
       ),
     );
   }
 }
-
-

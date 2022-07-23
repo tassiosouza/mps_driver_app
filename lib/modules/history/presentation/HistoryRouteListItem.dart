efdfa51d1f69c5b9fcb5034c32bb866fde68b490
josebiasneto <josebiasbsneto@gmail.com>
2022-07-23 14:48:08 -0300

    finish route item in history

--- lib/modules/history/presentation/HistoryRouteListItem.dart
+++ lib/modules/history/presentation/HistoryRouteListItem.dart
@@ -24,7 +24,8 @@ class HistoryRouteListItem extends StatelessWidget{
                       side: BorderSide(width: 1, color: App_Colors.grey_light.value)),
                   child: Container(child: IntrinsicHeight(child:
                   Row(children: [
-                    Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                    Flexible(child: Column(crossAxisAlignment: CrossAxisAlignment.start, 
+                        children: [
                       Text(title, style: TextStyle(fontSize: 14, color: App_Colors.primary_color.value,
                           fontWeight: FontWeight.w500)),
                       SizedBox(height: 10),
@@ -34,28 +35,54 @@ class HistoryRouteListItem extends StatelessWidget{
                           Image.asset('assets/images/verticaldots.png', height: 10, scale: 0.2),
                           Icon(Icons.location_on_outlined, color: Colors.black, size: 20)
                         ],),
-                        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                        Flexible(child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
                           Text("Meal Prep Sunday", style: TextStyle(fontSize: 12)),
                           SizedBox(height: 13),
-                          Text("202 Island Avenue, CA 92101", style: TextStyle(fontSize: 12))
-                        ],)
+                          Text("202 Island Avenue, CA 92101",
+                            style: TextStyle(fontSize: 12), overflow: TextOverflow.ellipsis,)
+                        ]))
                       ],),
                       SizedBox(height: 15),
                       Row(children: [
-                        SizedBox(width: 5),
-                        Icon(Icons.watch_later_outlined, size: 20),
+                        Icon(Icons.watch_later_outlined, size: 18),
                         SizedBox(width: 5),
                         Text("08:00", style: TextStyle(fontSize: 12)),
                         Text(" - ", style: TextStyle(fontSize: 12)),
                         Text("16:00", style: TextStyle(fontSize: 12)),
-                        SizedBox(width: 40),
-                        Icon(CustomIcon.bag_driver_icon, size: 20),
-                        SizedBox(width: 10),
+                        SizedBox(width: 30),
+                        Icon(CustomIcon.bag_driver_icon, size: 17),
+                        SizedBox(width: 5),
                         Text("22", style: TextStyle(fontSize: 12))
-                      ],)
-                    ]),
+                      ])
+                    ]), flex: 13),
                     VerticalDivider(thickness: 1, color: App_Colors.grey_dark.value),
-                    Column(children: [Text("teste")])]
+                    Flexible(child: Column(children: [
+                      Flexible(child: Column(children: [
+                        SizedBox(height: 8),
+                        Row(children: [
+                          Text("Distance", style: TextStyle(fontSize: 12,
+                          color: App_Colors.grey_text.value)),
+                          Text("26.2 m", style: TextStyle(fontSize: 12,
+                          color: App_Colors.grey_text.value))
+                        ], mainAxisAlignment: MainAxisAlignment.spaceBetween,),
+                        SizedBox(height: 10),
+                        Row(children: [
+                          Text("Time", style: TextStyle(fontSize: 12,
+                              color: App_Colors.grey_text.value)),
+                          Text("27min", style: TextStyle(fontSize: 12,
+                              color: App_Colors.grey_text.value))
+                        ], mainAxisAlignment: MainAxisAlignment.spaceBetween,),
+                      ],), flex: 6),
+                      Divider(thickness: 1, color: App_Colors.grey_dark.value,),
+                      Flexible(child: Column(children: [
+                        SizedBox(height: 20),
+                        Row(children: [
+                          Text("Total", style: TextStyle(fontSize: 12)),
+                          Text("\$40.00", style: TextStyle(fontSize: 12,
+                              color: App_Colors.primary_color.value, fontWeight: FontWeight.w500))
+                        ], mainAxisAlignment: MainAxisAlignment.spaceBetween,),
+                      ],), flex: 4)
+                    ]), flex: 7)]
                   )), padding: EdgeInsets.all(10)))))
         ]);
   }

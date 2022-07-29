8b5b1f355b352f7dfe3ef6c568cd1575226433e8
josebiasneto <josebiasbsneto@gmail.com>
2022-07-29 19:17:31 -0300

    build prepnews page

---
+++ lib/modules/prepnews/presentation/PrepNewsItem.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../theme/app_colors.dart';
+
+class PrepNewsItem extends StatefulWidget {
+  const PrepNewsItem({Key? key}) : super(key: key);
+
+  @override
+  State<PrepNewsItem> createState() => _PrepNewsItemState();
+}
+
+class _PrepNewsItemState extends State<PrepNewsItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(margin: EdgeInsets.all(20),
+        shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+    side: BorderSide(width: 1.0, color: App_Colors.grey_light.value)),
+        child: Container(child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(children: [
+            Container(padding: const EdgeInsets.all(2), margin: EdgeInsets.only(left: 10, right: 25),
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(80),
+                    border: Border.all(width: 1, color: App_Colors.grey_light.value)),
+                child: Icon(Icons.person, size: 50, color: App_Colors.grey_light.value)),
+            Column(children: [
+              Text("Meal Prep Sunday", style: TextStyle(fontWeight: FontWeight.w500,
+              fontSize: 14, color: App_Colors.black_text.value)),
+              SizedBox(height: 2),
+              Text("08:26 AM", style: TextStyle(color: App_Colors.grey_text.value))
+            ], crossAxisAlignment: CrossAxisAlignment.start)
+          ]),
+          SizedBox(height: 20),
+          Text("Meal Prep Sunday", style: TextStyle(color: App_Colors.primary_color.value,
+          fontWeight: FontWeight.w500)),
+          SizedBox(height: 10),
+          Text("Welcome to our new colaborator! Joseph Allen! Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC"),
+          SizedBox(height: 30)
+        ]), padding: EdgeInsets.all(15)));
+  }
+}
--- lib/modules/prepnews/presentation/PrepNewsPage.dart
+++ lib/modules/prepnews/presentation/PrepNewsPage.dart
@@ -1,4 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+
+import '../../../theme/app_colors.dart';
+import 'PrepNewsItem.dart';
 
 class PrepNewsPage extends StatefulWidget{
   @override
@@ -8,18 +13,21 @@ class PrepNewsPage extends StatefulWidget{
 class PrepNewsPageState extends State<PrepNewsPage>{
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Image(image: const AssetImage('assets/images/wip.png')),
-            const SizedBox(height: 30),
-            const Text("Wait while we are working on this feature",
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 20, fontFamily: 'Poppins')),
-          ]),
-    );
+    return SingleChildScrollView(child: Column(children: [
+      SizedBox(height: 80),
+      Row(children: [
+        Container(child: Text("Prep News", style: TextStyle(fontSize: 18,
+            fontWeight: FontWeight.w500)), padding: EdgeInsets.only(left: 20)),
+        Container(padding: const EdgeInsets.all(2), margin: EdgeInsets.only(right: 40),
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(80),
+                border: Border.all(width: 1, color: App_Colors.grey_light.value)),
+            child: Icon(Icons.person, size: 40, color: App_Colors.grey_light.value)),
+      ], mainAxisAlignment: MainAxisAlignment.spaceBetween),
+      SizedBox(height: 20),
+      Divider(thickness: 1, color: App_Colors.grey_light.value),
+      Container(child: ListView.builder(itemBuilder: (context, args) => PrepNewsItem(),
+        itemCount: 3, shrinkWrap: true, physics: NeverScrollableScrollPhysics(),), height: 900)
+    ]));
   }
 
 }

a6e7830d9f52288a545f276de2bc3cabf04c8929
Tassio Souza <souza.tassio@gmail.com>
2022-09-03 08:40:29 -0700

    fix download service for IOS

--- lib/Services/download_service.dart
+++ lib/Services/download_service.dart
@@ -1,25 +1,37 @@
+import 'dart:io';
+
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:path_provider/path_provider.dart';
 
-class DownloadService{
+class DownloadService {
   final url = "https://mundodasfigurinhas.com.br/tabela-copa-2022-download";
 
   Future<void> getDocument() async {
     await FlutterDownloader.registerCallback(HelpDownloadClass.callback);
     var status = await Permission.storage.request();
-    if(status.isGranted){
-      final baseStorage = await getExternalStorageDirectory();
-      if(baseStorage != null){
-        await FlutterDownloader.enqueue(url: url, savedDir: baseStorage.path,
-            showNotification: true, openFileFromNotification: true,
+    if (status.isGranted) {
+      var baseStorageAndroid;
+      var baseStorageIOS;
+      if (Platform.isAndroid) {
+        baseStorageAndroid = await getExternalStorageDirectory();
+      } else {
+        baseStorageIOS = await getApplicationSupportDirectory();
+      }
+      final path =
+          Platform.isAndroid ? baseStorageAndroid!.path : baseStorageIOS.path;
+      if (baseStorageAndroid != null) {
+        await FlutterDownloader.enqueue(
+            url: url,
+            savedDir: path,
+            showNotification: true,
+            openFileFromNotification: true,
             saveInPublicStorage: true);
       }
     }
   }
-
 }
 
-class HelpDownloadClass{
+class HelpDownloadClass {
   static void callback(String id, DownloadTaskStatus status, int progress) {}
 }
